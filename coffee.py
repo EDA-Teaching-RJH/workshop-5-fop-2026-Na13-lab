3efa903ff195cfd5029324b5d0ec0381d3239b88
Na13-lab <na13@kent.ac.uk>
2026-02-02 14:13:21 +0000

    Part 2 workshop 3

--- coffee.py
+++ coffee.py
@@ -1,1 +1,18 @@
-#Your code goes here
+amount_due = 75
+
+while amount_due > 0:
+    print("amount__due:", amount_due)
+    coin = int(input("Insert Coin:"))
+    
+    if coin in [50, 25, 10, 5, 1]:
+        amount_due -= coin
+        if amount_due < 0:
+            print("Thank you for your payment.Here is your change:", -amount_due)
+            change_owed = abs(amount_due)
+           
+            amount_due = 0
+        else:
+            print("Thank you for your payment. Amount due:", amount_due)
+    else:
+        print("Invalid coin. Please insert a valid coin.")
+        
